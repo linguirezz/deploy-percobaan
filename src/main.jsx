b7690ddd204c5da090e2219507ad3bd1315fd66a
ALNI <alinuriman000@gmail.com>
2024-05-24 10:30:52 +0700

    pemindahan coding sebelumnya ke repo baru

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,46 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { createBrowserRouter ,RouterProvider } from 'react-router-dom';
+
+import Home from './pages/home'
+import Project from './pages/project'
+import About from './pages/About'
+import Contact from './pages/Contact'
+
+
+
 import './index.css'
+
+
+const router = createBrowserRouter([
+  {
+    path:"/web-porto-2/",
+    element:<Home></Home>,
+  },
+  {
+    path:'/web-porto-2/project',
+    element:<Project/>
+  },
+  {
+    path:'/web-porto-2/about',
+    element:<About/>
+  },
+  {
+    path:'/web-porto-2/contact',
+    element:<Contact/>
+  }
+  
+  
+     
+    
+  
+  
+]
+
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,20 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        'primary':['Merriweather'],
+      
+      },
+      colors:{
+        primary : "#F6F6F6",
+        secondary : "#B3A197",
+        font : "#000000",
+        third : "#73654e",
+        fourth :"#ddd4d2",
+        fifth :"#121415",
+      }
+    },
   },
   plugins: [],
 }
